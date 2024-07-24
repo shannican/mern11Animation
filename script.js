76b2204438a29ad4815c8c4787ba45eda7c7357a
shannican <shannimura8683@gmail.com>
2024-07-24 10:36:58 +0530

    scrolltrigger

--- script.js
+++ script.js
@@ -1,54 +1,28 @@
-var tl = gsap.timeline()
-
-tl.from("#nav h2",{
-  y:-50,
-  duration:0.5,
-  delay:0.2,
-  opacity:0
-})
-
-tl.from("#nav h4",{
-  y:-50,
+gsap.from("#page2 h1",{
+  scale:1.3,
   opacity:0,
   duration:0.6,
-  stagger:0.2
+  scrollTrigger:{
+    trigger:"#page2 h1",
+    scroller:"body",
+    // markers:true,
+    start:"top 50%",
+    end:"top 40%",
+    scrub:2
+  }
 })
 
-tl.from("#center h1",{
+gsap.from(".card",{
+  y:130,
   opacity:0,
-  duration:0.5,
-  y:50
+  duration:0.8,
+  stagger:0.3,
+  scrollTrigger:{
+    trigger:".card",
+    scroller:"body",
+    // markers:true,
+    start:"top 70%",
+    end:"top 50%",
+    scrub:2
+  }
 })
-tl.from("#img3,#img2",{
-  y:-50,
-  opacity:0,
-  scale:0.5,
-  duration:0.5
-})
-tl.from("#center p",{
-  opacity:0,
-  duration:0.5,
-  y:50
-})
-tl.from("button",{
-  scale:1.3,
-  duration:0.5,
-  opacity:0
-})
-tl.from("#img1",{
-  x:-100,
-  opacity:0,
-  duration:0.3
-})
-tl.from("#img4",{
-  x:100,
-  opacity:0,
-  duration:0.3
-})
-tl.to("#img3,#img2",{
-  y:10,
-  duration:3,
-  repeat:-1,
-  yoyo:true
-})
-
